627c339d54a7f7a35fe115835c320bd50325e247
AlexanderSLoburev <121574459+AlexanderSLoburev@users.noreply.github.com>
2024-01-03 14:11:23 +0000

    intermediate commit

--- common/utils.erl
+++ common/utils.erl
@@ -1,5 +1,5 @@
 -module(utils).
--export([try_parse_number/1, is_digit/1]).
+-export([try_parse_number/1, is_digit/1, re_escape/1]).
 
 try_parse_number(String) ->
     case io_lib:fread("~d", String) of
@@ -9,3 +9,17 @@ try_parse_number(String) ->
 
 
 is_digit(Char) -> lists:member(Char, lists:seq($0, $9)).
+
+re_escape(Symbol) ->
+    case lists:member(Symbol, "[]\\^$.|?*+()") of
+        true ->
+            case is_integer(Symbol) of
+                true -> [$\ | [Symbol]];
+                false -> [$\ | Symbol]
+            end;
+        false ->
+            case is_integer(Symbol) of
+                true -> [Symbol];
+                false -> Symbol
+            end
+    end.
