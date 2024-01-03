3ee1bda64664fa06fa0070206c417c9913f02ffe
AlexanderSLoburev <121574459+AlexanderSLoburev@users.noreply.github.com>
2024-01-03 10:41:32 +0000

    ...

--- reverse_polish_notation_evaluator/src/reverse_polish_notation_evaluator.erl
+++ reverse_polish_notation_evaluator/src/reverse_polish_notation_evaluator.erl
@@ -1,5 +1,19 @@
 -module(reverse_polish_notation_evaluator).
 -export([start/0]).
 
+
+% evaluate_rpn(expression) ->
+
+
 start() ->
-   io:fwrite("Hello, world!~n").
+   io:fwrite("Hello, world!~n"),
+   Stack = stack:new(),
+   stack:push(3, stack:push(2, stack:push(1, Stack))),
+   io:fwrite("~p~n", stack:peek(Stack)),
+   io:fwrite("~p~n", stack:pop(Stack)),
+   io:fwrite("~p~n", stack:peek(Stack)),
+   io:fwrite("~p~n", stack:pop(Stack)),
+   io:fwrite("~p~n", stack:peek(Stack)),
+   io:fwrite("~p~n", stack:pop(Stack)),
+   io:fwrite("~p~n", stack:peek(Stack)),
+   io:fwrite("~p~n", stack:is_empty(Stack)).
